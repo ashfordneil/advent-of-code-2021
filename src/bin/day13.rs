700dfae46932e575c8dc479d2934069eae244fe8
Neil Ashford <me@neilashford.dev>
2021-12-13 15:49:13 +1000

    Day 13 complete!
    I've noticed that the parsing code is getting more and more verbose as
    we go through the month. I think I'm going to introduce some extra
    utilities, just to make things cleaner, and do a bit of a refactor
    before the next day.
    
    Also, I think the hardest (or at least most doubt-inducing) part of
    today's challenge was reading the output for part 2. I'm not good at
    Captchas on the best of days.

---
+++ src/bin/day13.rs
@@ -0,0 +1,214 @@
+use advent_of_code_2021::util;
+use std::{collections::HashSet, fs, path::PathBuf, str::FromStr};
+
+use itertools::Itertools;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Args {
+    #[structopt(default_value = "./data/day-thirteen.txt")]
+    /// The path to the input file we want to run with.
+    file: PathBuf,
+}
+
+type Coordinate = (usize, usize);
+
+#[derive(Debug)]
+struct Manual {
+    points: Vec<Coordinate>,
+    folds: Vec<Fold>,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Fold {
+    Y(usize),
+    X(usize),
+}
+
+impl FromStr for Manual {
+    type Err = eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (points, folds) = s
+            .split_once("\n\n")
+            .ok_or_else(|| eyre::format_err!("Invalid input"))?;
+        let points = points
+            .lines()
+            .map(|line| line.trim())
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let (x, y) = line
+                    .split_once(',')
+                    .ok_or_else(|| eyre::format_err!("Invalid coordinate: {:?}", line))?;
+                let x = x.parse()?;
+                let y = y.parse()?;
+                Ok::<_, eyre::Report>((x, y))
+            })
+            .collect::<Result<Vec<Coordinate>, _>>()?;
+        let folds = folds
+            .lines()
+            .map(|line| line.trim())
+            .filter(|line| !line.is_empty())
+            .map(|line| line.parse())
+            .collect::<Result<Vec<Fold>, _>>()?;
+
+        let output = Manual { points, folds };
+        Ok(output)
+    }
+}
+
+impl FromStr for Fold {
+    type Err = eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tail = s
+            .strip_prefix("fold along ")
+            .ok_or_else(|| eyre::format_err!("Invalid fold instruction"))?;
+        let (dir, val) = tail
+            .split_once('=')
+            .ok_or_else(|| eyre::format_err!("Missing ="))?;
+        let val = val.parse()?;
+        let output = match dir {
+            "x" => Fold::X(val),
+            "y" => Fold::Y(val),
+            _ => eyre::bail!("Invalid axis to fold along"),
+        };
+        Ok(output)
+    }
+}
+
+fn main() -> eyre::Result<()> {
+    let args = util::setup::<Args>()?;
+    let input = fs::read_to_string(args.file)?;
+    let input = input.parse()?;
+
+    println!("Part one: {}", part_one(&input));
+    println!("Part two: {:#?}", part_two(&input));
+
+    Ok(())
+}
+
+impl Manual {
+    pub fn points<'a>(&'a self) -> impl 'a + Iterator<Item = Coordinate> {
+        self.points.iter().copied()
+    }
+
+    pub fn folds<'a>(&'a self) -> impl 'a + Iterator<Item = Fold> {
+        self.folds.iter().copied()
+    }
+}
+
+impl Fold {
+    fn reflect(val: usize, fold: usize) -> usize {
+        if val < fold {
+            val
+        } else if val > fold {
+            fold - (val - fold)
+        } else {
+            unreachable!("They said nothing would appear on the folding line")
+        }
+    }
+
+    pub fn fold(self, (x, y): Coordinate) -> Coordinate {
+        match self {
+            Fold::X(fx) => (Self::reflect(x, fx), y),
+            Fold::Y(fy) => (x, Self::reflect(y, fy)),
+        }
+    }
+
+    pub fn get_x(self) -> Option<usize> {
+        if let Fold::X(inner) = self {
+            Some(inner)
+        } else {
+            None
+        }
+    }
+
+    pub fn get_y(self) -> Option<usize> {
+        if let Fold::Y(inner) = self {
+            Some(inner)
+        } else {
+            None
+        }
+    }
+}
+
+fn part_one(input: &Manual) -> usize {
+    let first_fold = input.folds[0];
+    input
+        .points()
+        .map(|coord| first_fold.fold(coord))
+        .unique()
+        .count()
+}
+
+fn part_two(input: &Manual) -> Vec<String> {
+    let all_points = input
+        .points()
+        .map(|coord| input.folds().fold(coord, |coord2, fold| fold.fold(coord2)))
+        .collect::<HashSet<_>>();
+
+    let max_x = input.folds().filter_map(Fold::get_x).last().unwrap();
+    let max_y = input.folds().filter_map(Fold::get_y).last().unwrap();
+
+    (0..max_y)
+        .map(|y| {
+            (0..max_x)
+                .map(|x| {
+                    if all_points.contains(&(x, y)) {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                })
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::Manual;
+
+    const EXAMPLE_INPUT: &'static str = r"
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+
+        fold along y=7
+        fold along x=5
+    ";
+
+    fn get_input() -> Manual {
+        EXAMPLE_INPUT.parse().unwrap()
+    }
+
+    #[test]
+    fn example_part_one() {
+        assert_eq!(17, super::part_one(&get_input()));
+    }
+
+    #[test]
+    fn example_part_two() {
+        let output = super::part_two(&get_input());
+        assert_eq!(
+            &["#####", "#...#", "#...#", "#...#", "#####", ".....", "....."],
+            &output[..]
+        );
+    }
+}
